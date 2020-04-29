10001e2f9ae2a4caaaf82ef1f398f3da104b9e3a
abdelilah <naimi201@hotmail.com>
2020-04-29 19:17:51 -0400

    Ajoutez des fichiers projet.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public  string Street { get; set; }
+        public int Number { get; set; }
+    }
+}
